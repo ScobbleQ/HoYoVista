13e2fca5fa2f6e2784615d9cb90669e0ac793d67
Jason <c04jason@icloud.com>
2024-12-31 19:30:30 -0500

    Create cron.js

---
+++ src/class/cron.js
@@ -0,0 +1,41 @@
+import { CronJob } from 'cron';
+import logger from '../utils/logger.js';
+
+/**
+ * Cron class to handle cron jobs
+ */
+export class Cron {
+    #jobs = [];
+
+    constructor() {
+        this.#jobs = [];
+    }
+
+    /**
+     * Add a new job to the cron
+     * @param {string} time - Cron time format
+     * @param {*} callback - Function to be called
+     * @param  {...any} args - Arguments for the function
+     */
+    addJob(time, callback, ...args) {
+        const jobCallback = () => callback(...args);
+        const job = new CronJob(time, jobCallback, null, false, 'America/New_York');
+        this.#jobs.push(job);
+    }
+
+    /**
+     * Start all cron jobs
+     */
+    startJobs() {
+        logger.info(`Cron: Starting ${this.#jobs.length} jobs`);
+        this.#jobs.forEach(job => job.start());
+    }
+
+    /**
+     * Stop all cron jobs
+     */
+    stopJobs() {
+        logger.info(`Cron: Stopping ${this.#jobs.length} jobs`);
+        this.#jobs.forEach(job => job.stop());
+    }
+}
