4afe11a9949e52dedcbf884bb7bebcec63f7ca1e
Jason <c04jason@icloud.com>
2024-12-31 19:29:38 -0500

    deleted

--- bot/buttons/delete_button.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	data: {
-		id: 'delete_button',
-		description: 'Deletes the message attached to the button',
-	},
-	async execute(interaction) {
-		await interaction.message.delete();
-	},
-};
